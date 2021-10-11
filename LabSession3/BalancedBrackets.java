059a68c736068739b94d86c46f45e7355b7ea2ec
Telomelonia <88445101+Telomelonia@users.noreply.github.com>
2021-10-11 22:11:41 +0530

    Add files via upload

---
+++ LabSession3/BalancedBrackets.java
@@ -0,0 +1,70 @@
+package LabSession3;
+import java.util.*;
+public class BalancedBrackets {
+
+	static boolean balanced(String str)
+	{
+		// Using ArrayDeque rather than using Stack class
+		Deque<Character>stack = new ArrayDeque<Character>();
+		// Traversing the string
+		for(int i=0;i<str.length();i++)
+		{
+			char x = str.charAt(i);
+			
+			if(x == '(' || x=='[' || x == '{') {
+				// Push the element in the stack
+				stack.push(x);
+				continue;
+			}
+			/*If current character is not opening bracket, 
+			then it must be closing -> stack will not be empty*/
+			
+			if(stack.isEmpty())
+			{
+				return false;
+			}
+			char target;
+			
+			switch(x) 
+			{
+			case')':
+				target = stack.pop();
+				if(target == '{' || target == '[')
+					return false;
+				break;
+				
+			case '}':
+				target = stack.pop();
+				if(target == '(' || target == ']')
+					return false;
+				break;
+				
+			case ']' :
+				target = stack.pop();
+				if(target == '(' || target == '{')
+					return false;
+				break;
+			}
+			
+		}
+		return (stack.isEmpty());
+	}
+	
+
+
+	//Driver code
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		if(balanced(str))
+		{
+			System.out.println("Brackets are balanced");
+		}
+		else
+		{
+			System.out.println("Not balanced");
+		}
+
+	}
+
+}
---
+++ LabSession3/LongestPath.java
@@ -0,0 +1,74 @@
+package LabSession3;
+
+import java.util.*;
+
+class LongestPath
+{
+	static class Node 
+	{
+		Node leftNode;
+		Node rightNode;
+		int nodeData;
+	};
+	static Node newNode(int nodeData) 
+    {
+		Node temp = new Node();
+		temp.nodeData = nodeData;
+		temp.leftNode = null;
+		temp.rightNode = null;
+		return temp;
+	}
+	public static ArrayList<Integer> LongestPath(Node root) {
+
+		// If root = null return a empty set 
+		if (root == null) {
+			ArrayList<Integer> output = new ArrayList<>();
+			return output;
+		}
+		// Recursion of root.rightNode
+		ArrayList<Integer> rightNode = LongestPath(root.rightNode);
+
+		// Recursion of root.leftNode
+		ArrayList<Integer> leftNode = LongestPath(root.leftNode);
+
+		// size comparison of the two ArrayList
+        //according to which current node is inserted
+		if (rightNode.size() < leftNode.size()) 
+        {
+			leftNode.add(root.nodeData);
+		}    
+        else 
+        {
+			rightNode.add(root.nodeData);
+		}
+		// Return desired ArrayList
+		return (leftNode.size() > rightNode.size() ? leftNode : rightNode);
+	}
+
+
+
+
+
+	// Driver Code
+	public static void main(String[] args) 
+    {
+		Node root = newNode(100);
+		root.leftNode = newNode(20);
+		root.rightNode = newNode(130);
+		root.leftNode.leftNode = newNode(10);
+		root.leftNode.rightNode = newNode(50);
+		root.rightNode.leftNode = newNode(110);
+		root.rightNode.rightNode = newNode(140);
+		root.leftNode.leftNode.leftNode = newNode(5);
+
+
+		ArrayList<Integer> output = LongestPath(root);
+		int size = output.size();
+
+		System.out.print(output.get(size - 1));
+		for (int i = size - 2; i >= 0; i--) 
+        {
+			System.out.print(" -> " + output.get(i));
+		}
+	}
+}
